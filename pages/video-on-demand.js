657a2063e060c5da34f0a4b8bfa39b6878739f55
Theerapat <blend.theerapat@gmail.com>
2020-12-22 13:20:51 +0700

    video-on-demand page

--- pages/video-on-demand.js
+++ pages/video-on-demand.js
@@ -2,7 +2,10 @@ import MainLayout from '../layouts/main'
 import styled from 'styled-components'
 import Container from '../components/Container'
 import font from '../helpers/font'
-import { Tag } from '../components'
+import { Checkbox } from '../components'
+import Router from 'next/router'
+import { Divider, Select, Row, Col } from 'antd'
+const { Option } = Select
 
 const Wrapper = styled('div')`
   background-color: #E5E5E5;
@@ -11,16 +14,6 @@ const Wrapper = styled('div')`
 const Content = styled('div')`
   display: flex;
   align-items: flex-start;
-`
-
-const FilterWrapper = styled('div')`
-  flex: 1;
-`
-
-const CourseWrapper = styled('div')`
-  flex: 4;
-  display: flex;
-  flex-direction: column;
 `
 
 const Title = styled('div')`
@@ -56,28 +49,64 @@ const CourseContent = styled('div')`
   margin-left: 12px;
 `
 
+const BoldTitle = styled('div')`
+  font-size: 16px;
+  font-family: ${font.bold};
+  color: #333333;
+  margin-top: 24px;
+`
+
+const FilterTitle = styled('div')`
+  font-size: 16px;
+`
+
+const FilterItem = styled('div')`
+  margin: 16px 0;
+`
+
 const VideoOnDemandPage = () => {
   return (
     <MainLayout>
       <Wrapper>
         <Container paddingTop='32px'>
-          <Content>
-            <FilterWrapper>
-              <Title>Video on demand</Title>
-            </FilterWrapper>
-            <CourseWrapper>
-              {
-                new Array(6).fill(null).map(item => (
-                  <CourseCard>
-                    <CourseImage src='/static/images/deep-learning.png' />
-                    <CourseContent>
-                      <CourseTitle>เทคโนโลยีรีไซเคิลฝุ่นสังกะสีจากอุตสาหกรรมชุบเคลือบ สังกะสีแบบจุ่มร้อน (Hot-Dip</CourseTitle>
-                    </CourseContent>
-                  </CourseCard>
-                ))
-              }
-            </CourseWrapper>
-          </Content>
+          <Row gutter={24}>
+            <Col xs={0} md={6}>
+              <Title>คอร์สเรียน</Title>
+              <BoldTitle>การจัดเรียง</BoldTitle>
+              <Select defaultValue='desc' style={{marginTop: '8px', width: '100%'}}>
+                <Option value='desc'>ใหม่สุด</Option>
+                <Option value='asc'>เก่าสุด</Option>
+              </Select>
+              <BoldTitle>คัดกรอง</BoldTitle>
+              <FilterTitle>รูปแบบการเรียน</FilterTitle>
+                <Checkbox>คอร์สเรียนที่สถาบัน</Checkbox> <br />
+                <Checkbox>คอร์สออนไลน์</Checkbox>
+              <Divider style={{marginTop: '16px', marginBottom: '0'}}/>
+              <FilterItem>
+                <FilterTitle>หมวดหมู่</FilterTitle>
+                <Checkbox>เหมืองแร่</Checkbox> <br />
+                <Checkbox>อุตสาหกรรม</Checkbox>
+              </FilterItem>
+              <Divider style={{margin: 0}} />
+              <FilterItem>
+                <FilterTitle>รับรองใบประกาศนียบัตร</FilterTitle>
+                <Checkbox>รับรอง</Checkbox> <br />
+                <Checkbox>ไม่รับรอง</Checkbox>
+              </FilterItem>
+            </Col>
+            <Col xs={24} md={18}>
+            {
+              new Array(6).fill(null).map(item => (
+                <CourseCard>
+                  <CourseImage src='/static/images/deep-learning.png' />
+                  <CourseContent>
+                    <CourseTitle>เทคโนโลยีรีไซเคิลฝุ่นสังกะสีจากอุตสาหกรรมชุบเคลือบ สังกะสีแบบจุ่มร้อน (Hot-Dip</CourseTitle>
+                  </CourseContent>
+                </CourseCard>
+              ))
+            }
+            </Col>
+          </Row>
         </Container>
       </Wrapper>
     </MainLayout>
