2d211d0e3bda598b3231e571c5f656ded0ee368a
Theerapat <blend.theerapat@gmail.com>
2020-12-24 10:44:38 +0700

    register page

---
+++ pages/register/1.js
@@ -0,0 +1,74 @@
+import MainLayout from '../../layouts/main'
+import { Container } from '../../components'
+import font from '../../helpers/font'
+import styled from 'styled-components'
+import { Form, Input, Card } from 'antd'
+
+const PageTitle = styled('div')`
+  font-family: ${font.bold};
+  font-size: 24px;
+  color: #00937B;
+  line-height: 1;
+`
+
+const Title = styled('div')`
+  font-family: ${font.bold};
+  font-size: 20px;
+  margin-top: 24px;
+  line-height: 1;
+`
+
+const RegisterStepOnePage = () => {
+  const [form] = Form.useForm()
+
+  const handleSubmit = (values) => {
+    console.log('handleSubmit', values)
+  }
+
+  return (
+    <MainLayout>
+      <Container maxWidth='780px' paddingTop='32px' paddingBottom='150px'>
+        <PageTitle>สมัครสมาชิก</PageTitle>
+        <Title>ข้อมูลการเข้าสู่ระบบ</Title>
+        <Form
+          onFinish={handleSubmit}
+          form={form}
+          style={{marginTop: '16px'}}
+        >
+          <Form.Item 
+            label="อีเมล"
+            name='email'
+            labelCol={{ span: 24 }}
+            rules={[{ required: true, message: 'กรุณากรอกอีเมล' }, { type: 'email' } ]}
+          >
+            <Input placeholder='กรุณากรอกอีเมล' />
+          </Form.Item>
+          <Form.Item 
+            label="รหัสผ่าน"
+            name='note_id'
+            labelCol={{ span: 24 }}
+            rules={[{ required: true, message: 'กรุณากรอกรหัสผ่าน' }]}
+          >
+            <Input placeholder='กรุณากรอกรหัสผ่าน' />
+          </Form.Item>
+          <Card>
+            รหัสผ่านประกอบด้วย <br />
+          - มีความยาวอย่างน้อย 8 ตัวอักษร <br />
+          - มีทั้งตัวเลขและตัวอักษรภาษาอังกฤษ <br />
+          - มีตัวพิมพ์เล็กและตัวพิมพ์ใหญ่อย่างน้อยอย่างละ 1 ตัว
+          </Card>
+          <Form.Item 
+            label="ยืนยันรหัสผ่าน"
+            name='note_id'
+            labelCol={{ span: 24 }}
+            rules={[{ required: true, message: 'กรุณายืนยันรหัสผ่าน' }]}
+          >
+            <Input placeholder='กรุณายืนยันรหัสผ่าน' />
+          </Form.Item>
+        </Form>
+      </Container>
+    </MainLayout>
+  )
+}
+
+export default RegisterStepOnePage
---
+++ pages/register/2.js
@@ -0,0 +1,241 @@
+import MainLayout from '../../layouts/main'
+import { Container } from '../../components'
+import font from '../../helpers/font'
+import styled from 'styled-components'
+import { Form, Input, Radio, Row, Col, DatePicker, Select, Checkbox } from 'antd'
+const { Option } = Select
+const CheckboxGroup = Checkbox.Group
+
+const PageTitle = styled('div')`
+  font-family: ${font.bold};
+  font-size: 24px;
+  color: #00937B;
+  line-height: 1;
+`
+
+const Title = styled('div')`
+  font-family: ${font.bold};
+  font-size: 20px;
+  margin-top: 24px;
+  line-height: 1;
+`
+
+const RegisterStepTwoPage = () => {
+  const [form] = Form.useForm()
+
+  const handleSubmit = (values) => {
+    console.log('handleSubmit', values)
+  }
+
+  return (
+    <MainLayout>
+      <Container maxWidth='780px' paddingTop='32px' paddingBottom='150px'>
+        <PageTitle>สมัครสมาชิก</PageTitle>
+        <Title>ข้อมูลส่วนตัว</Title>
+        <Form
+          onFinish={handleSubmit}
+          form={form}
+          style={{marginTop: '16px'}}
+        >
+          <Row gutter={16}>
+            <Col xs={12}>
+              <Form.Item 
+                label="ชื่อจริงภาษาไทย"
+                name='firstname'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณากรอกอีเมล' }]}
+              >
+                <Input placeholder='กรุณากรอกอีเมล' />
+              </Form.Item>
+            </Col>
+            <Col xs={12}>
+              <Form.Item 
+                label="นามสกุลภาษาไทย"
+                name='lastname'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณากรอกอีเมล' }]}
+              >
+                <Input placeholder='กรุณากรอกอีเมล' />
+              </Form.Item>
+            </Col>
+            <Col xs={12}>
+              <Form.Item 
+                label="ชื่อจริงภาษาอังกฤษ"
+                name='firstname_en'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณากรอกอีเมล' }]}
+              >
+                <Input placeholder='กรุณากรอกอีเมล' />
+              </Form.Item>
+            </Col>
+            <Col xs={12}>
+              <Form.Item 
+                label="นามสกุลภาษาอังกฤษ"
+                name='lastname_en'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณากรอกอีเมล' }]}
+              >
+                <Input placeholder='กรุณากรอกอีเมล' />
+              </Form.Item>
+            </Col>
+            <Col xs={12}>
+              <Form.Item 
+                label="เลขบัตรประชาชน"
+                name='id_card'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณากรอกอีเมล' }]}
+              >
+                <Input placeholder='กรุณากรอกอีเมล' />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <Form.Item 
+                label="เพศ"
+                name='email'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณากรอกอีเมล' }]}
+              >
+                <Radio.Group>
+                  <Radio value={1}>ชาย</Radio>
+                  <Radio value={4}>หญิง</Radio>
+                </Radio.Group>
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <Form.Item 
+                label="วันเกิด"
+                name='email'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณากรอกอีเมล' }, { type: 'email' } ]}
+              >
+                <DatePicker style={{width: '100%' }} />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col xs={12}>
+              <Form.Item 
+                label="จังหวัด"
+                name='email'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณาเลือกจังหวัด' } ]}
+              >
+                <Select placeholder='เลือกจังหวัด'>
+                  <Option>กรุงเทพมหานคร</Option>
+                  <Option>สมุทรปราการ</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col xs={12}>
+              <Form.Item 
+                label="เขต/อำเภอ"
+                name='email'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณาเลือกเขต/อำเภอ' } ]}
+              >
+                <Select placeholder='เลือกเขต/อำเภอ'>
+                  <Option>กรุงเทพมหานคร</Option>
+                  <Option>สมุทรปราการ</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col xs={12}>
+              <Form.Item 
+                label="แขวง/ตำบล"
+                name='email'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณากรอกอีเมล' }]}
+              >
+                <Select placeholder='เลือกแขวง/ตำบล'>
+                  <Option>กรุงเทพมหานคร</Option>
+                  <Option>สมุทรปราการ</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col xs={12}>
+              <Form.Item 
+                label="รหัสไปรษณีย์"
+                name='email'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณาเลือกรหัสไปรษรีย์' }]}
+              >
+                <Input placeholder='เลือกรหัสไปรษรีย์' />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Form.Item 
+            label="รายละเอียดที่อยู่ (ห้องเลขที่, บ้านเลขที่, ตึก, ชื่อถนน)"
+            name='email'
+            labelCol={{ span: 24 }}
+            rules={[{ required: true, message: 'กรุณากรอกรายละเอียด' } ]}
+          >
+            <Input placeholder='กรอกรายละเอียด' />
+          </Form.Item>
+          <Row gutter={16}>
+            <Col xs={12}>
+              <Form.Item 
+                label="เบอร์โทรศัพท์"
+                name='phone'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณากรอกเบอร์โทรศัพท์' } ]}
+              >
+                <Input placeholder='กรอกเบอร์โทรศัพท์' />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Form.Item 
+            label="วุฒิการศึกษาสูงสุด"
+            name='email'
+            labelCol={{ span: 24 }}
+            rules={[{ required: true, message: 'กรุณาเลือกวุฒิการศึกษาสูงสุด' }, { type: 'email' } ]}
+          >
+            <Radio.Group>
+              <Radio value={1}>ต่ำกว่าปริญญาตรี</Radio>
+              <Radio value={4}>ปริญญาตรี</Radio>
+              <Radio value={4}>ปริญญาโท</Radio>
+              <Radio value={4}>ปริญญาเอก</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Row gutter={16}>
+            <Col xs={12}>
+              <Form.Item 
+                label="อาชีพ"
+                name='job'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณาเลือกอาชีพ' } ]}
+              >
+                <Select placeholder='เลือกอาชีพ'>
+                  <Option>d</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col xs={12}>
+              <Form.Item 
+                label="ตำแหน่ง"
+                name='position'
+                labelCol={{ span: 24 }}
+                rules={[{ required: true, message: 'กรุณากรอกตำแหน่ง' } ]}
+              >
+                <Input placeholder='กรอกตำแหน่ง' />
+              </Form.Item>
+            </Col>
+            <Form.Item 
+              label="รู้จักเราผ่านทางช่องทางใด"
+              name='where'
+              labelCol={{ span: 24 }}
+              rules={[{ required: true, message: 'กรุณาเลือกช่องทางที่รู้จัก' } ]}
+            >
+              <CheckboxGroup options={['สื่อออนไลน์', 'เพื่อนแนะนำ', 'หนังสือพิมพ์', 'สื่อโทรทัศน์', 'อื่น ๆ']} />
+            </Form.Item>
+          </Row>
+        </Form>
+      </Container>
+    </MainLayout>
+  )
+}
+
+export default RegisterStepTwoPage
