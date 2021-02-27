aace5f5400a94d3ab68ac49bf9d7a12e7fd0154a
OoI3enzZaoO <benz_5300@hotmail.com>
2021-02-27 22:24:52 +0700

    Change API name

--- pages/course/[courseId]/learn.js
+++ pages/course/[courseId]/learn.js
@@ -334,7 +334,7 @@ const LearnPage = ({
         <div>{timeConvert(obj.time)}</div>
         </VideoTitle>
         <Progress
-          percent={obj.done_lesson ? 100 : obj.video_position || 0}
+          percent={obj.done_lesson ? 100 : obj.video_progress || 0}
           showInfo={false}
           strokeWidth={2}
           strokeColor={{
