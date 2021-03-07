3f8ad09caf9e41bc77129362fbe12e59c66db46e
OoI3enzZaoO <benz_5300@hotmail.com>
2021-03-07 21:45:55 +0700

    fix close onfinishmodal

--- components/modals/FinishedVideoModal.js
+++ components/modals/FinishedVideoModal.js
@@ -1,15 +1,22 @@
 import { Modal, Button } from 'antd'
 import styled from 'styled-components'
 
+
+
 const FinishedVideoModal = ({
   isOpen = false,
-  onSubmit = () => { }
+  onSubmit = () => {},
+  onClose = () => {}
 }) => {
+  const closeModal = () => {
+    onClose()
+  }
   return (
     <Modal
       visible={isOpen}
       footer={null}
       bodyStyle={{ padding: '50px 24px 16px 24px',  }}
+      onCancel={() => closeModal()}
     >
       <Button style={{width: '100%'}} onClick={() => onSubmit()}>ถัดไป</Button>
     </Modal>
