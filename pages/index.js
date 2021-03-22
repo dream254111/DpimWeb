c6c0901e48f5527e6936c4a9be4edb60d81f4447
Harizadoz <harizadoz@gmail.com>
2021-03-22 18:29:23 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -119,22 +119,22 @@ margin:0 0 20px;
 ${maxWidth.lg`
   width: 100%;
   height: 160px;
-margin:0 0 20px;
+margin:20px 0 20px;
 
 `};${maxWidth.custom`
   width: 100%;
   height: 190px;
-margin:0 0 50px;
+margin:20px 0 50px;
 
 `};${maxWidth.custom1`
   width: 100%;
   height: 160px;
-margin:0 0 50px;
+margin:20px 0 50px;
 
 `};${maxWidth.custom2`
   width: 100%;
   height: 160px;
-margin:0 0 50px;
+margin:20px 0 50px;
 
 `};
 
