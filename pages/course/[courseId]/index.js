f3eaa9ce5595c3ee10d130938991344eb55d485b
Cashew <54216995+OoI3enzZaoO@users.noreply.github.com>
2021-05-04 19:18:38 +0700

    Fix Scroll Position on Course Page (#24)

--- pages/course/[courseId]/index.js
+++ pages/course/[courseId]/index.js
@@ -728,20 +728,48 @@ const CourseDetailPage = ({ courseId, memberToken, memberDetail }) => {
   const [isExtendTimeLoading, setIsExtendTimeLoading] = useState(false)
   const [isVoucherModalOpen, setIsVoucherModalOpen] = useState(false)
 
-  const executeScroll1 = () => {
-    scrollRef1.current.scrollIntoView({behavior: "smooth"})
-  }
-  const executeScroll2 = () => {
-    scrollRef2.current.scrollIntoView({behavior: "smooth"})
-  }
-  const executeScroll3 = () => {
-    scrollRef3.current.scrollIntoView({behavior: "smooth"})
-  }
-  const executeScroll4 = () => {
-    scrollRef4.current.scrollIntoView({behavior: "smooth"})
-  }
-  const executeScroll5 = () => {
-    scrollRef5.current.scrollIntoView({behavior: "smooth"})
+
+  const scrollToTarget = (scrollRef) => {
+    const headerOffset = 70
+    //getBoundingClientRect() will get value of window(only visible page) not document(whole page)
+    switch (scrollRef) {
+      case scrollRef1:
+        const offsetPosition1 = scrollRef1.current.getBoundingClientRect().top - headerOffset + window.scrollY
+        window.scrollTo({
+          top: offsetPosition1,
+          behavior: 'smooth'
+        })
+        break
+      case scrollRef2:
+        const offsetPosition2 = scrollRef2.current.getBoundingClientRect().top - headerOffset + window.scrollY
+        window.scrollTo({
+          top: offsetPosition2,
+          behavior: 'smooth'
+        })
+        break
+      case scrollRef3:
+        const offsetPosition3 = scrollRef3.current.getBoundingClientRect().top - headerOffset + window.scrollY
+        window.scrollTo({
+          top: offsetPosition3,
+          behavior: 'smooth'
+        })
+        break
+      case scrollRef4:
+        const offsetPosition4 = scrollRef4.current.getBoundingClientRect().top - headerOffset + window.scrollY
+        window.scrollTo({
+          top: offsetPosition4,
+          behavior: 'smooth'
+        })
+        break
+      case scrollRef5:
+        const offsetPosition5 = scrollRef5.current.getBoundingClientRect().top - headerOffset + window.scrollY
+        window.scrollTo({
+          top: offsetPosition5,
+          behavior: 'smooth'
+        })
+        break
+      default: null
+    }
   }
 
   useEffect(() => {
@@ -935,15 +963,15 @@ const CourseDetailPage = ({ courseId, memberToken, memberDetail }) => {
         <TabBar>
           <Container>
           <TabMenu>
-            <TabItem onClick={executeScroll1}>ภาพรวมคอร์ส</TabItem>
+            <TabItem onClick={() => scrollToTarget(scrollRef1)}>ภาพรวมคอร์ส</TabItem>
             <Line />
-            <TabItem onClick={executeScroll2}>วัตถุประสงค์</TabItem>
+            <TabItem onClick={() => scrollToTarget(scrollRef2)}>วัตถุประสงค์</TabItem>
             <Line />
-            <TabItem onClick={executeScroll3}>เนื้อหาในคอร์ส</TabItem>
+            <TabItem onClick={() => scrollToTarget(scrollRef3)}>เนื้อหาในคอร์ส</TabItem>
             <Line />
-            <TabItem onClick={executeScroll4}>ประโยชน์ที่ผู้เรียนจะได้รับ</TabItem>
+            <TabItem onClick={() => scrollToTarget(scrollRef4)}>ประโยชน์ที่ผู้เรียนจะได้รับ</TabItem>
             <Line />
-            <TabItem onClick={executeScroll5}>สอบถามเกี่ยวกับหลักสูตร</TabItem>
+            <TabItem onClick={() => scrollToTarget(scrollRef5)}>สอบถามเกี่ยวกับหลักสูตร</TabItem>
           </TabMenu>
           </Container>
         </TabBar>
